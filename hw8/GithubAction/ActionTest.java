3f3ea0ad85570697aa28860254605b145bc3c033
swufe_lgs <swufe_lgs@outlook.com>
2022-06-12 15:59:17 +0800

    hw8

---
+++ hw8/GithubAction/ActionTest.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActionTest {
+
+    public static void main(String[] args) {
+
+        List arrayList = new ArrayList();
+        arrayList.add("总有刁民想害朕");
+        arrayList.add(7);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            Object item = arrayList.get(i);
+            if (item instanceof String) {
+                String str = (String) item;
+                System.out.println("泛型测试 item = " + str);
+            } else if (item instanceof Integer) {
+                Integer inte = (Integer) item;
+                System.out.println("泛型测试 item = " + inte);
+            }
+        }
+
+    }
+
+}
